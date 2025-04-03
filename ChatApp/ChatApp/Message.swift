a3bb456abeafefc808abe66b79466f42fe46566a
TaehanYoon <yunk222@naver.com>
2025-04-03 17:13:42 +0900

    4/3 Firebase Hosting, 채팅앱

---
+++ ChatApp/ChatApp/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by 윤태한 on 4/3/25.
+//
+
+
+import Foundation
+
+struct Message {
+    let id: String
+    let text: String
+    let senderId: String
+    let senderEmail: String
+    let timestamp: TimeInterval
+}
