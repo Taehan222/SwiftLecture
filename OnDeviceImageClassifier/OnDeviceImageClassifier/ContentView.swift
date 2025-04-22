20303ee29b4ef5c02c1dd9ab2b45db51c2d24995
TaehanYoon <yunk222@naver.com>
2025-04-22 16:33:22 +0900

    4/21 온디바이스 이미지 인식

---
+++ OnDeviceImageClassifier/OnDeviceImageClassifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnDeviceImageClassifier
+//
+//  Created by 윤태한 on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnDeviceImageClassifier/OnDeviceImageClassifier/OnDeviceImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDeviceImageClassifierApp.swift
+//  OnDeviceImageClassifier
+//
+//  Created by 윤태한 on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct OnDeviceImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
